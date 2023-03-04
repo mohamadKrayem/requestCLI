b6b79ac1dffba17615af1ca2651f81ef057f9892
Mohamad Krayem <mk2242682@gmail.com>
2023-03-04 23:15:31 +0200

    define a get command with a -s --secure flag to ensure if the passed url starts with http:// or https://

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,13 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"os"
-
+	"github.com/mohamadkrayem/requestCLI/cmd/get"
 	"github.com/spf13/cobra"
+	"os"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -46,6 +43,5 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(get.GetCmd)
 }
-
-
